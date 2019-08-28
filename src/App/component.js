3d74b40f77caa0035525606320235a9dabbd8b25
viktoryia.budzionnaya.epam <Viktoryia_Budzionnaya@epam.com>
2019-08-28 12:24:57 +0300

    add error boundary

--- src/App/component.js
+++ src/App/component.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Header, Footer } from './../shared';
 import MovieSearch from './../pages/MovieSearch';
 import MovieDetails from './../pages/MovieDetails';
+import ErrorBoundary from './../components/ErrorBoundary';
 
 import styles from './style.module.scss';
 
@@ -12,12 +13,14 @@ function App() {
     <>
       <Header />
       <main className={styles.container}>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={MovieSearch} />
-            <Route exact path="/:id" component={MovieDetails} />
-          </Switch>
-        </Router>
+        <ErrorBoundary>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={MovieSearch} />
+              <Route exact path="/:id" component={MovieDetails} />
+            </Switch>
+          </Router>
+        </ErrorBoundary>
       </main>
       <Footer />
     </>
---
+++ src/components/ErrorBoundary/component.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import styles from './style.module.scss';
+
+class ErrorBoundary extends Component {
+  state = {
+    hasError: false,
+    errorName: '',
+    componentStack: '',
+  };
+
+  componentDidCatch(error, info) {
+    this.setState({
+      hasError: true,
+      errorName: error.toString(),
+      componentStack: info.componentStack,
+    });
+  }
+
+  render() {
+    const { errorName, componentStack, hasError } = this.state;
+    const formattedComponentStack = (componentStack + '').trim().split('\n');
+
+    return hasError ? (
+      <div className={styles.error}>
+        <h1 className={styles.title}>Oops! An error occured!</h1>
+        <h2 className={styles.subtitle}>Here is what we know:</h2>
+        <p className={styles.name}>
+          <span className={styles.label}>Error:</span>
+          <span>{errorName}</span>
+        </p>
+        <article>
+          <h3 className={styles.label}>Stacktrace:</h3>
+          {formattedComponentStack.map((el, i) => (
+            <p key={'trace' + i} className={styles.stacktrace}>
+              {el}
+            </p>
+          ))}
+        </article>
+      </div>
+    ) : (
+      this.props.children
+    );
+  }
+}
+
+export default ErrorBoundary;
