950cdd0e776301ca636a0eabd5eb43b9d887e18c
Viktoryia Budzionnaya <Viktoryia_Budzionnaya@epam.com>
2020-07-02 19:57:18 +0300

    small fix

--- src/components/MovieCard/component.js
+++ src/components/MovieCard/component.js
@@ -9,28 +9,26 @@ const MovieCard = props => {
   const { title, description, year, genre, picLink, rating, duration } = props;
 
   return (
-    <>
-      <div className={styles.card}>
-        <div className={styles.header}>
-          <img className={styles.img} src={picLink} height="400" alt={title}/>
-        </div>
-        <div className={styles.body}>
-          <div className={styles.line}>
-            <h2 className={styles.title}>
-              {title}
-              <span className={styles.rating}> {rating} </span>
-            </h2>
-          </div>
-          <div className={styles.subline}>
-            <span className={styles.genre}> {genre} </span>
-            <span className={styles.year}> {year} </span>
-            <span className={styles.duration}> {duration} </span>
-          </div>
-          <p className={styles.description}>{description}</p>
-          <LinkButton to="/">Back</LinkButton>
-        </div>
+    <div className={styles.card}>
+      <div className={styles.header}>
+        <img className={styles.img} src={picLink} height="400" alt={title}/>
       </div>
-    </>
+      <div className={styles.body}>
+        <div className={styles.line}>
+          <h2 className={styles.title}>
+            {title}
+            <span className={styles.rating}> {rating} </span>
+          </h2>
+        </div>
+        <div className={styles.subline}>
+          <span className={styles.genre}> {genre} </span>
+          <span className={styles.year}> {year} </span>
+          <span className={styles.duration}> {duration} </span>
+        </div>
+        <p className={styles.description}>{description}</p>
+        <LinkButton to="/">Back</LinkButton>
+      </div>
+    </div>
   );
 };
 
